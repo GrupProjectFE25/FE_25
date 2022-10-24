0e28d3044e3fe71c9c7ae0a68bac53b77c9e677d
FaizalRajib <muhfaizalrajib@gmail.com>
2022-10-25 00:41:39 +0800

    kelar

---
+++ asset/js/script-gitar.js
@@ -0,0 +1,51 @@
+const BASE_URL = "https://63547f47e64783fa8285a355.mockapi.io/Gitar";
+
+getGitar(BASE_URL)
+
+function getGitar(url){
+    fetch(url).then(result => result.json())
+    .then(data =>  {
+        // console.log(data);
+        showGitar(data);
+    })
+}
+
+function showGitar(data){
+    let main = '';
+    data.forEach(gitar => {
+        console.log(gitar);
+        let {title, img}  = gitar
+        main +=`
+        <div class="col mb-4">
+                <div class="card h-100">
+                    <img src="${img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <a href="#"><button type="button" class="btn btn-primary" style="font-size:12px ;">Selengkapnya...</button></a>
+                    </div>
+                </div>
+            </div>
+        `
+        document.getElementById('list-materi').innerHTML = main
+    });
+}
+
+let search = document.getElementById('search')
+
+function getSearch(request){
+    let url = `${BASE_URL}?title=${request}`
+    fetch(url).then(result => result.json())
+    .then(data =>  {
+        // console.log(data);
+        showGitar(data);   
+    })
+}
+
+search.addEventListener('change', (e) => {
+    if (e.target.value == "") {
+        getGitar(BASE_URL);
+    } else {
+        getSearch(e.target.value)
+    }
+    // e.target.value == "" ? getMovie(url) : getSearch(e.target.value) 
+})
---
+++ asset/js/script-jenis-seni.js
@@ -0,0 +1,51 @@
+const BASE_URL = "https://63547f47e64783fa8285a355.mockapi.io/Jenis-senirupa";
+
+getJenis(BASE_URL)
+
+function getJenis(url){
+    fetch(url).then(result => result.json())
+    .then(data =>  {
+        // console.log(data);
+        showJenis(data);
+    })
+}
+
+function showJenis(data){
+    let main = '';
+    data.forEach(unsurSeniRupa => {
+        console.log(unsurSeniRupa);
+        let {title,img}  = unsurSeniRupa
+        main +=`
+        <div class="col mb-4">
+                <div class="card h-100">
+                    <img src="${img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <a href="#"><button type="button" class="btn btn-primary" style="font-size:12px ;">Selengkapnya...</button></a>
+                    </div>
+                </div>
+            </div>
+        `
+        document.getElementById('list-materi').innerHTML = main
+    });
+}
+
+let search = document.getElementById('search')
+
+function getSearch(request){
+    let url = `${BASE_URL}?title=${request}`
+    fetch(url).then(result => result.json())
+    .then(data =>  {
+        // console.log(data);
+        showJenis(data);   
+    })
+}
+
+search.addEventListener('change', (e) => {
+    if (e.target.value == "") {
+        getJenis(BASE_URL);
+    } else {
+        getSearch(e.target.value)
+    }
+    // e.target.value == "" ? getMovie(url) : getSearch(e.target.value) 
+})
